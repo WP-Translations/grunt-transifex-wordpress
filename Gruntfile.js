235ccf1166fa2d2f2aeb4561521d7438161874e7
fxbenard <fxbenard@gmail.com>
2015-03-31 23:45:37 +0200

    Force pull

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
 				command: 'npm update'
 			},
 			txpull: { // Pull Transifex translation - grunt exec:txpull
-				cmd: 'tx pull -a --minimum-perc=100' // Change the percentage with --minimum-perc=yourvalue
+				cmd: 'tx pull -a -f --minimum-perc=100' // Change the percentage with --minimum-perc=yourvalue
 			},
 			txpush_s: { // Push pot to Transifex - grunt exec:txpush_s
 				cmd: 'tx push -s'
