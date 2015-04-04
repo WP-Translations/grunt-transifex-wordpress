5a6bd06168ce6704a954a7714af3ff7bbf6d0dd8
fxbenard <fxbenard@gmail.com>
2015-04-04 14:36:59 +0200

    Add transifex actions

--- grunt/exec.js
+++ grunt/exec.js
@@ -4,5 +4,14 @@ module.exports = {
 	update_po_wti: {
 		cmd: 'wti pull',
 		cwd: '<%= pkg.theme.domainpath %>',
-	}
+	},
+        npmUpdate: {
+        command: 'npm update'
+      },
+      txpull: { // Pull Transifex translation - grunt exec:txpull
+        cmd: 'tx pull -a --minimum-perc=100' // Change the percentage with --minimum-perc=yourvalue
+      },
+      txpush_s: { // Push pot to Transifex - grunt exec:txpush_s
+        cmd: 'tx push -s'
+      }
 };
